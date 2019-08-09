592cc826dca2765ef1d802c8de578a39d10ec3bc
Carlos Vladimir Esmeralda Ponce <carlosv@Carloss-iMac.local>
2019-08-09 18:00:55 -0400

    iniciando con bootstrap

--- src/components/Temperatura.js
+++ src/components/Temperatura.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { WiDaySunny } from "weather-icons-react";
+import { Button } from 'react-bootstrap';
 
 function Temperatura(props){
     return (
         <div className="Temperatura">
             <h1><WiDaySunny size={24} color='#2f2f2f' />Temperatura: {props.datos}</h1>
+            <Button variant="primary">Primary</Button>
         </div>
     )
 }
